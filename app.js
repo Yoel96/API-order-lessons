659fde6d6ecd03579803686cacfa14f1a9d3b6f3
Yoel Betancor <juyobema@gmail.com>
2024-03-04 15:32:29 +0000

    AUTH -SIgnUp function created -Login function created

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ require('dotenv').config()
 const express= require('express')
 const app= express()
 const {dbSync, dbCheck} = require('./api/db/sync')
+const DBRelationships = require('./api/db/relationships')
+
 
 
 
 
 const startServer = async()=>{
 
+    app.use(express.json())
+    app.use('/api', require('./api/routes/index.js'))
+
+
+
     app.listen(process.env.PORT, async (err)=>{
 
         if(err) throw new Error(err)
         await dbCheck()
-        await dbSync()
+        await DBRelationships()
+        //await dbSync()
         console.log('*'.repeat(100))
         console.log(`Server listening on port ${process.env.PORT}`)
         console.log('*'.repeat(100))
