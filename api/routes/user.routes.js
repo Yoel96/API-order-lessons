2e0c81ccd52ebcae75e9a83a9247d295e1e52037
yoel <juyobema@gmail.com>
2024-04-04 18:48:38 +0100

    fixed added teacher role to get user

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,7 +3,7 @@ const {getAllUsers, getUser, deleteUser, updateUser} = require('../controllers/u
 const {checkRole, checkAuth} = require('../middlewares/auth.middleware')
 
 router.get('/', checkAuth,(req,res,next)=>{checkRole(req, res,next,["admin"])}, getAllUsers)
-router.get('/:userId', checkAuth,(req,res,next)=>{checkRole(req, res,next,["admin,teacher"])}, getUser)
+router.get('/:userId', checkAuth,(req,res,next)=>{checkRole(req, res,next,["admin","teacher"])}, getUser)
 router.delete('/:userId', checkAuth,(req,res,next)=>{checkRole(req, res,next,["admin"])}, deleteUser)
 router.put('/:userId', checkAuth,(req,res,next)=>{checkRole(req, res,next,["admin"])}, updateUser)
 
